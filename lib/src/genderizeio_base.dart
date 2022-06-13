fef85149688473c09890f07dd9490dd587713cdd
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-06-14 04:12:58 +0700

    fix: change error message

--- lib/src/genderizeio_base.dart
+++ lib/src/genderizeio_base.dart
@@ -32,12 +32,12 @@ class Genderize {
 class GenderizeAPI {
   Future<Genderize> send(String name) async {
     final response = await http
-        .get(Uri.parse("https://api.genderize.io?name=${name}"));
+        .get(Uri.parse('https://api.genderize.io?name=${name}'));
 
     if (response.statusCode == 200) {
       return Genderize.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load gender');
     }
   }
 }
